7291b1e009c4d1ab301c116ed33b3a21bb769e10
aitp-t-sato <tomohiro.sato@itp.agaroot.co.jp>
2024-04-01 19:05:03 +0900

    updated_atの型の修正

--- lib/assets/messages.sql
+++ lib/assets/messages.sql
@@ -3,5 +3,5 @@ CREATE TABLE messages (
     content VARCHAR(1000) NOT NULL comment '内容',
     user_id VARCHAR(80) NOT NULL comment 'ユーザーID',
     created_at DATETIME comment '登録日時' DEFAULT current_timestamp NOT NULL,
-    updated_at VARCHAR(80) comment '更新日時' DEFAULT current_timestamp ON UPDATE current_timestamp NOT NULL
+    updated_at DATETIME comment '更新日時' DEFAULT current_timestamp ON UPDATE current_timestamp NOT NULL
 );
