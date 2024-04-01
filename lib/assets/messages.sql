0a37e2403e3c27730ba7af350a827bfc9feed504
aitp-t-sato <tomohiro.sato@itp.agaroot.co.jp>
2024-04-01 19:02:58 +0900

    IDの自動採番

--- lib/assets/messages.sql
+++ lib/assets/messages.sql
@@ -1,5 +1,5 @@
 CREATE TABLE messages (
-    ID BIGINT UNSIGNED PRIMARY KEY comment 'ID',
+    ID BIGINT UNSIGNED PRIMARY KEY AUTO_INCREMENT comment 'ID',
     content VARCHAR(1000) NOT NULL comment '内容',
     user_id VARCHAR(80) NOT NULL comment 'ユーザーID',
     created_at DATETIME comment '登録日時' DEFAULT current_timestamp NOT NULL,
